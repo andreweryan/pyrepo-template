3f568f8b368cd23f16443c999f042c6588323c8b
andreweryan <andrrvt15@gmail.com>
2024-01-11 23:16:24 -0500

    update

--- package/main.py
+++ package/main.py
@@ -1,13 +1,13 @@
 import platform
+
 """
-This is a template. 
+This is a template.
 """
 
+
 def main():
-    a = [{'':'','':'','':'','':'', '':''}]
     print(f"Hello, World! Your OS is: {platform.platform()}")
 
 
 if __name__ == "__main__":
     main()
-    
